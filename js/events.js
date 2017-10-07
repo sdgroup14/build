3762c47a78683b10f7828d3ba7be166648c4206d
Daniel Soloviov <sdgroup14@gmail.com>
2017-10-08 01:32:50 +0300

    end desktop

--- js/events.js
+++ js/events.js
@@ -1,3 +1,8 @@
+var $preloader = $('.preloader-container'),
+    $svg_anm   = $preloader.find('#bodymovin');
+$svg_anm.delay(2500).fadeOut();
+$preloader.delay(3000).fadeOut('slow');
+
 $('.current-lang').on('click', function(e) {
   $('.current-lang').toggleClass('lang-active ');
   $('.lang-toggle').toggleClass('toggle-active');
@@ -12,8 +17,6 @@ $(document).on('click', function(e) {
 
 var swiper = new Swiper('.build-type', {
   slidesPerView: 3,
-  // spaceBetween: 130,
-  // freeMode: true,
   nextButton: ".next-slide",
   prevButton: ".prev-slide",
   onSlideChangeStart: function() {
@@ -24,10 +27,242 @@ var swiper = new Swiper('.build-type', {
   },
 });
 
-$('.menu-item').not('.lang').on('mouseenter', function(){
-	$(this).addClass('active-menu-item');
-	$('.menu-item').not(this).css('opacity', '.55');
-}).on('mouseleave', function(){
-	$('.menu-item').removeClass('active-menu-item');
-	$('.menu-item').css('opacity', '');
+$('.menu-item').not('.lang').on('mouseenter', function() {
+  $(this).addClass('active-menu-item');
+  $('.menu-item').not(this).css('opacity', '.55');
+}).on('mouseleave', function() {
+  $('.menu-item').removeClass('active-menu-item');
+  $('.menu-item').css('opacity', '');
 });
+
+function OpenCall(btn) {
+  btn.addClass('active').attr('data-call-open', '1');
+  $('.call-content').addClass('active');
+  setTimeout(function() {
+    $('.call-btn-txt').addClass('active');
+
+  }, 300)
+}
+
+function CloseCall() {
+  $('.call-btn-txt').removeClass('active');
+  $('.call-content').removeClass('active');
+
+  setTimeout(function() {
+    $('.call-ico').removeClass('active');
+  }, 300)
+}
+
+$('.call-ico').on('click', function() {
+  OpenCall($(this));
+});
+
+$('.call-close').on('click', function() {
+  CloseCall();
+});
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
+
+$('body').on('click', '.plan-number-item', function() {
+  $(this).toggleClass('checked');
+  if ($(this).attr('data-item-check') != 1) {
+    $(this).attr('data-item-check', '1');
+  } else {
+    $(this).attr('data-item-check', '0');
+  }
+
+  var visible_item_type,
+    data_number = $(this).attr('data-number'),
+    data_type = $(this).attr('data-type'),
+    item_type = $('.plan-type-item[data-plan-type="' + data_type + '"]'),
+    item_number = $('.plan-marker[data-number="' + data_number + '"]');
+
+  if (item_number.attr('data-visible') != 1) {
+    item_number.show().attr('data-visible', '1');
+  } else if (item_number.attr('data-visible') == 1) {
+    item_number.hide().attr('data-visible', '0');
+  }
+
+  visible_item_type = $('.plan-marker[data-visible="1"]').filter('[data-type="' + data_type + '"]');
+
+  if (item_type.attr('data-check') != 1 && visible_item_type.length) {
+    item_type.addClass('active-not-all').attr('data-check', '1');
+  } else if (item_type.attr('data-check') == 1 && !visible_item_type.length) {
+    item_type.removeClass('active-not-all').attr('data-check', '0');
+  }
+
+  if (visible_item_type.length == $('.plan-number-item[data-type="' + data_type + '"]').length) {
+    item_type.removeClass('active-not-all');
+    item_type.addClass('active-all');
+
+  } else if (visible_item_type.length != $('.plan-number-item[data-type="' + data_type + '"]').length && visible_item_type.length) {
+    item_type.addClass('active-not-all');
+    item_type.removeClass('active-all');
+  } else if (visible_item_type.length != $('.plan-number-item[data-type="' + data_type + '"]').length && !visible_item_type.length) {
+    item_type.removeClass('active-not-all');
+    item_type.removeClass('active-all');
+  }
+});
+
+$('body').on('click', '.plan-type-item', function(e) {
+  e.preventDefault();
+  $(this).toggleClass('active-all').removeClass('active-not-all');
+  var data_type = $(this).attr('data-plan-type'),
+    this_number_items = $('.plan-number-item[data-type="' + data_type + '"]'),
+    this_markers = $('.plan-marker[data-type="' + data_type + '"]');
+
+  if ($('.plan-number-item[data-type="' + data_type + '"]').length !== $('.plan-number-item[data-type="' + data_type + '"]').filter('[data-item-check="1"]').length) {
+    this_number_items.addClass('checked').attr('data-item-check', '1');
+    this_markers.show().attr('data-visible', '1');
+  } else {
+    this_number_items.removeClass('checked').attr('data-item-check', '0');
+    this_markers.hide().attr('data-visible', '0');
+  }
+});
+
+$('.btn-type-add').on('click', function() {
+  $('.plan-number-item').addClass('checked').attr('data-item-check', '1');
+  $('.plan-marker').show().attr('data-visible', '1');
+  $('.plan-type-item').addClass('active-all').attr('data-check', '1');
+});
+
+$('.btn-type-clear').on('click', function() {
+  $('.plan-number-item').removeClass('checked').attr('data-item-check', '0');
+  $('.plan-marker').hide().attr('data-visible', '0');
+  $('.plan-type-item').removeClass('active-all').attr('data-check', '0');
+});
+
+
+
+
+
+var photo_l = $('.gallery-thumbs').find('.swiper-slide').length;
+
+var galleryTop = new Swiper('.gallery-top', {
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  spaceBetween: 0,
+
+  slidesPerView: 1,
+});
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 0,
+  centeredSlides: true,
+  touchRatio: 0.2,
+  slidesPerView: photo_l,
+  slideToClickedSlide: true
+});
+
+galleryTop.params.control = galleryThumbs;
+galleryThumbs.params.control = galleryTop;
+
+
+$(document).on('scroll', function() {
+  // var st = $(this).scrollTop();
+  var top = $(document).scrollTop();
+  // if (top >= 100) {
+  //   $('.call-btn-container').addClass('move-to-bottom');
+  // } else {
+  //   $('.call-btn-container').removeClass('move-to-bottom');
+  // }
+
+  if (top > $('.s4').offset().top && $('.call-ico').attr('data-call-open') != 1) {
+    $('.call-btn-container').addClass('move-to-bottom');
+    setTimeout(function() {
+      OpenCall($('.call-ico'));
+    }, 300)
+  }
+  // else if (top < $('.s4').offset().top && $('.call-ico').attr('data-call-open') == 1) {
+  //   CloseCall();
+  //   setTimeout(function() {
+  //     $('.call-btn-container').removeClass('move-to-bottom');
+  //   }, 500)
+
+  // }
+});
+
+$('.menu-item a[href^="#"]').on("click.smoothscroll", function(e) {
+  e.preventDefault();
+  var i = this.hash,
+    t = $(i);
+  $("html, body").animate({ scrollTop: t.offset().top }, 500, "swing", function() {
+    window.location.hash = i;
+  });
+});
+
+var plan_marker;
+
+$.ajax({
+  dataType: 'json',
+  url: 'json/plan-data.json',
+  success: function(data) {
+    console.log(data);
+    for (var i = 0; data.length > i; i++) {
+      $('.plan-map-container').append('<div class="plan-marker" data-type="' + data[i].type + '" data-number="' + data[i].number + '" style="left:' + data[i].x + 'px; top: ' + data[i].y + 'px;"><span class="plan-marker-number">' + data[i].number + '</span></div>');
+      // <div class="plan-type-item" data-plan-type="type-1"> <label> <input class="checkbox" type="checkbox" name="checkbox-type"> <span class="checkbox-custom"></span> <span class="label-txt">Тип 1 | 255,15 м2</span> </label> </div>
+      $('.plan-number').append('<div class="plan-number-item" data-type="' + data[i].type + '" data-number="' + data[i].number + '">' + data[i].number + '</div>');
+    }
+  }
+});
+
+
+
+
+
+
+// $(function() {
+
+
+//   setTimeout(function() {
+//     wheelzoom($('.plan-map-container'), { zoom: 0.1, maxZoom: 10 });
+//   }, 1000);
+
+// });
+
+
+// $(function(){
+//   var minZoom = 1;
+//   var maxZoom = 5;
+//   var $zoomP = $('.zoom-in');
+//   var $zoomM = $('.zoom-out');
+//   var $zoomDiv = $('.plan-map-container');
+//   $zoomP.on('click', function(e){
+//     zoom(1);
+//   });
+//   $zoomM.on('click', function(e){
+//     zoom(-1);
+//   });
+//   function zoom(num) {
+//     var zoomVal = parseInt($zoomDiv.css('zoom'), 10);
+//     zoomVal = zoomVal + num;
+//     if (zoomVal < minZoom || zoomVal > maxZoom) {
+//       return;
+//     }
+//     $zoomDiv.css('zoom', zoomVal);
+//     // $zoomVal.html(zoomVal);
+//   }
+// });
+
+
+(function() {
+  // var $section = $('#inverted-contain');
+  $('.plan-map-container').panzoom({
+    $zoomIn: $(".zoom-in"),
+    $zoomOut: $(".zoom-out"),
+    // $zoomRange: $section.find(".zoom-range"),
+    // $reset: $section.find(".reset"),
+    startTransform: 'scale(1)',
+    increment: 0.1,
+    minScale: 1,
+    contain: 'invert'
+  })
+  // .panzoom('zoom');
+})();
