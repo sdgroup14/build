fcc11e2a5ef833817f1c23846fbd840a1865d169
Daniel <sdgroup14@gmail.com>
2017-10-23 23:44:15 +0300

    23.10.17 - DanBro
    - change footer logo ( s10  block )
    - change font-size footer ( footer block )
    - centered bg-img in video ( s1 block )
    - plan add infrastructure btn ( s5 block )
    - fix call btn in mob. ver. ( call popup )
    
    - del async in template ( project module )
    - swipe by keyup ( s6 block )
    -change menu header bg-color ( menu block )

--- src/index.php
+++ src/index.php
@@ -85,135 +85,15 @@
       </div>
     </div>
   </div>
+  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js"></script>
-
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbuSjSAIif0CB0GNuob2bfG1jd1y9FdxE&language=uk&region=UA&callback=initMap"></script>
   <!-- build:remove -->
   <script src="js/lib/jquery.panzoom.min.js"></script>
   <script type="text/javascript" src="js/events.js"></script>
   <!-- /build -->
   <!-- build:include ../templates/js-script.html -->
   <!-- /build -->
-
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbuSjSAIif0CB0GNuob2bfG1jd1y9FdxE&language=uk&region=UA&callback=initMap"></script>
-    <script>
-        function initMap() {
-            var directionsDisplay;
-            var directionsService;
-            var directionsMap;
-            var startCoords = {lat: false, lng: false};
-            var endCoords = {lat: 50.3971129, lng: 29.9776504};
-            var kyivCoords = {lat: 50.4474833, lng: 30.2409454};
-            var kyiv = new google.maps.LatLng(kyivCoords);
-            var start = kyiv;
-            var end = new google.maps.LatLng(endCoords);
-            var distanceInfo = {lat: 50.437127, lng: 30.111913};
-
-            console.log(startCoords);
-
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    startCoords = {lat: position.coords.latitude, lng: position.coords.longitude};
-                });
-            } else {
-                alert('Ваш браузер не поддерживает геолокацию');
-            }
-
-            if (startCoords.lat != false){
-                start = new google.maps.LatLng(startCoords);
-                document.querySelector('#gmapLink').href = 'https://www.google.com.ua/maps/dir/'+startCoords.lat+','+startCoords.lng+'/Коттеджный+городок+Севериновка,+Киевская+область';
-            } else {
-                start = new google.maps.LatLng(kyivCoords);
-            }
-
-            console.log(startCoords);
-
-            function getDirection() {
-                var mapOptions = {
-                    zoom: 11,
-                    center: start,
-                    gestureHandling: 'cooperative',
-                    mapTypeControl: false,
-                    zoomControl: true,
-                    zoomControlOptions: {
-                        position: google.maps.ControlPosition.RIGHT_CENTER,
-                        style: google.maps.ZoomControlStyle.LARGE
-                    },
-                    fullscreenControl: false,
-                    streetViewControl: false
-                };
-                directionsMap = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-                directionsService = new google.maps.DirectionsService();
-                directionsDisplay = new google.maps.DirectionsRenderer({
-                    draggable: true,
-                    map: directionsMap,
-                    suppressMarkers: true
-                });
-
-                directionsDisplay.addListener('directions_changed', function() {
-                    // This is where you would do your custom processing
-                    computeTotalDistance(directionsDisplay.getDirections());
-                });
-
-                calcRoute();
-            }
-
-            function displayRoute(start, end, service, display) {
-                service.route({
-                    origin: start,
-                    destination: end,
-                    travelMode: google.maps.TravelMode.DRIVING,
-                    avoidTolls: true
-                }, function(response, status) {
-                    if (status === google.maps.DirectionsStatus.OK) {
-                        display.setDirections(response);
-                    } else {
-                        alert('Could not display directions due to: ' + status);
-                    }
-                });
-            }
-
-            function computeTotalDistance(result) {
-                var total = 0;
-                var myroute = result.routes[0];
-                for (var i = 0; i < myroute.legs.length; i++) {
-                    total += myroute.legs[i].distance.value;
-                }
-                total = total / 1000;
-                console.log(total + ' км');
-                var infoWindow = new google.maps.InfoWindow({map: directionsMap});
-                infoWindow.setPosition(kyivCoords);
-                infoWindow.setContent(total + ' км<br />до Києва');
-            }
-
-            function calcRoute() {
-                var request = {
-                    origin: start,
-                    destination: end,
-                    travelMode: 'DRIVING'
-                };
-                directionsService.route(request, function(result, status) {
-                    if (status == google.maps.DirectionsStatus.OK) {
-                        directionsDisplay.setDirections(result);
-                    }
-                    new google.maps.Marker({
-                        position: startCoords,
-                        map: directionsMap,
-                        icon: '/img/markerUser.png',
-                        title: 'Это Вы'
-                    });
-                    new google.maps.Marker({
-                        position: endCoords,
-                        map: directionsMap,
-                        icon: '/img/markerSeverynivka.png',
-                        title: 'Севериновка'
-                    });
-                });
-            }
-
-            getDirection();
-        }
-    </script>
 </body>
 </html>
